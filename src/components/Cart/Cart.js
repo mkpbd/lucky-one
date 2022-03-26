319f2ea8765c36c8bf6034efbf520875ed1a9016
mkpbd <mostofakamal1990@gmail.com>
2022-03-26 11:30:20 +0600

    add delte button

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import {Card,Button} from 'react-bootstrap';
+import {Card,Button, Row, Col} from 'react-bootstrap';
 import './Cart.css'
+import {FaTrashAlt} from 'react-icons/fa'
 
 const Cart = (props) => {
     const {img, name, id} = props.cart;
+  //  const {removeProductFormCart} = props.removeProductFormCart;
     return (
         <>
-                <div className='d-flex mt-2 mx-2'>
-                    <img src={img} alt="not support image" className='img-thumbnail img' />
-                    <h3>{name}</h3>
-                    <h6>icon not found</h6>
-                </div>
+                <Row className='my-2'>
+                    <Col sm={4}>
+                      <img src={img} alt="not support image" className='img-thumbnail img' />
+                    </Col>
+                    <Col sm={6}>
+                        <h3 className='h-100'>{name}</h3>
+                    </Col>
+                    <Col sm={2}>
+                        <Button className='btn btn-md btn-danger' onClick={()=> props.removeProductFormCart(id)}>
+                            <FaTrashAlt></FaTrashAlt>
+                        </Button>
+                    </Col>
+                </Row>
+                
           
             
       </>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
+
 import React, { useEffect, useState } from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import Cart from "../Cart/Cart";
 import Product from '../Product/Product';
 const Shop = () => {
@@ -16,15 +17,31 @@ const Shop = () => {
         .then(data => setProducts(data));
     } , [])
 
-
+// product add to Cart
     const addToCartHeandler = (product)=>{
       const newCart = [...cart, product ];
 
       setCart(newCart);
 
-      console.log(cart);
+      //console.log(cart);
     }
 
+    // product Remove form Cart
+
+
+    const removeProductFormCart = (productId) => {
+
+      const proId =   cart.find(c => c.id === productId);
+
+        delete cart[proId];
+
+        console.log(cart, "delete prodid")
+
+        setCart(cart);
+
+
+
+    }
 
 
   return (
@@ -37,18 +54,29 @@ const Shop = () => {
               <Row className="row-cols-1 row-cols-md-3 g-4">
                   {/* dyname data go hear */}
                     {
-                        products.map(product => <Product product={product} key={product.id} addToCartHeandler={addToCartHeandler}></Product>)
+                        products.map(product => <Product product={product} key={product.id} addToCartHeandler={addToCartHeandler} ></Product>)
                     }
                  
               </Row>
           </Col>
           <Col sm={4}>
             
-                    <Card>
+                    <Card className="px-2">
                     {
-                      cart.map((cart) => <Cart cart={cart}></Cart>)
+                      cart.map((cart) => <Cart cart={cart} key={cart.id} removeProductFormCart={removeProductFormCart}></Cart>)
                     }
+                  
             </Card>
+            
+                <Row className="py-5">
+                    <Col className="col-sm-4 offset-4">
+                    <Button className='btn btn-md btn-suscess'>Choise For Me</Button>
+           
+                    </Col>
+                    <Col className="col-sm-4 offset-4 mt-3">
+                    <Button className='btn btn-md btn-warning'>Change Agin</Button>
+                    </Col>
+                </Row>
             
             </Col>
         </Row>
